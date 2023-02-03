99695b3d7954f5098369ac514e89ac7e6024cd3c
Thainara Tabile <thainara.tabilebenevides@gmail.com>
2023-02-03 12:52:06 -0300

    inicio HTML e CSS

--- src/main.js
+++ src/main.js
@@ -1,6 +1,4 @@
-import { example } from './data.js';
-// import data from './data/lol/lol.js';
-import data from './data/pokemon/pokemon.js';
-// import data from './data/rickandmorty/rickandmorty.js';
+import allFiltres from '.data';
+import data from '.lol';
 
-console.log(example, data);
+
