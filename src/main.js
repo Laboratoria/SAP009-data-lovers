5484a75c5e8b051133c5b19005b82915fbadec0c
Sarah R. Vieira <sarahrdvieira@gmail.com>
2023-02-08 22:15:00 -0300

    Título, data de lançamento e pôster do filme aparecem na página em divs

--- src/main.js
+++ src/main.js
@@ -1,10 +1,41 @@
 import data from "./data/ghibli/ghibli.js";
+const allAnimations = data.films;
+
+function showAnimations(allAnimations) {
+  return allAnimations.map((animation) =>
+  `
+  <div class="cards">
+  <img class="posters" src="${animation.poster}" alt="Pôster do filme">
+  <p class="film-info">${animation.title} - ${animation.release_date}</p>
+  </div>
+  `).join("");
+}
+document.querySelector(".animation_cards").innerHTML = showAnimations(allAnimations);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 //document.getElementById("list-container").innerHTML = showAll(data);
 
-document.querySelector(".animation_cards").innerHTML = showInfo(data);
+/*document.querySelector(".animation_cards").innerHTML = showInfo(data);
 
-/*function showInfo(data) {
+function showInfo(data) {
   const allAnimations = [];
   const allAnimationsPoster = [];
   for (let i = 0; i < data.films.length; i++) {
@@ -18,18 +49,5 @@ document.querySelector(".animation_cards").innerHTML = showInfo(data);
           </ul>`;
 }*/
 
-function showInfo(data) {
-  const allAnimations = [];
-  const allAnimationsPosters = [];
-  for (let i = 0; i < data.films.length; i++) {
-    const animationPoster = data.films[i].poster;
-    const animationTitle = data.films[i].title;
-    const animationRelease = data.films[i].release_date;
-    allAnimations.push(animationTitle + " - " + animationRelease);
-    allAnimationsPosters.push(animationPoster); 
-  }
-  return `<ul>
-            ${allAnimations.map((item) => `<li>${item}</li>`).join("")}
-          </ul>`
-}
+
 
