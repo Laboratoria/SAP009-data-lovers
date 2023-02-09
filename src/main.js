49651cac5cbcbdc3c413355f55e8278fee8ab9ee
marianalopesdev <marianalopes.dev@gmail.com>
2023-02-09 02:27:54 -0300

    js changes / card exhibition changes

--- src/main.js
+++ src/main.js
@@ -1,41 +1,71 @@
+//import { example } from "./data.js";
 import data from "./data/ghibli/ghibli.js";
+const allAnimations = data.films;
 
-//document.getElementById("list-container").innerHTML = showAll(data);
+document.querySelector(".animation_cards").innerHTML =
+  showAnimations(allAnimations);
 
-document.querySelector(".animation_cards").innerHTML = showInfo(data);
-
-/*function showInfo(data) {
-  const allAnimations = [];
-  const allAnimationsPoster = [];
-  for (let i = 0; i < data.films.length; i++) {
-    const animationPoster = data.films[i].poster
-    const animationTitle = data.films[i].title;
-    const animationRelease = data.films[i].release_date;
-    allAnimations.push(animationTitle + " - " + animationRelease);
-  }
-  return `<ul>
-            ${allAnimations.map((item) => `<li>${item}</li>`).join("")}
-          </ul>`;
-}*/
-
-function showInfo(data) {
-  const allAnimations = [];
-  const allAnimationsPosters = [];
-  for (let i = 0; i < data.films.length; i++) {
-    const animationPoster = data.films[i].poster;
-    const animationTitle = data.films[i].title;
-    const animationRelease = data.films[i].release_date;
-    allAnimations.push(animationTitle + " - " + animationRelease);
-    allAnimationsPosters.push(animationPoster); 
-   
-  }
-  console.log(allAnimationsPosters);
-  return `<ul>
-            ${allAnimations.map((item) => `<li>${item}</li>`).join("")}
-          </ul>
-          <div>
-          ${allAnimationsPosters.map((item) => `<li>${item}</li>`).join("")}
-          </div>
-          `
+function showAnimations(allAnimations) {
+  return allAnimations
+    .map(
+      (animation) =>
+        `
+  <div class="cards">
+  <img class="posters" src="${animation.poster}" alt="Pôster do filme">
+  <p class="film-info">${animation.title} </p>
+  <p class="film-info"> ${animation.release_date}</p>
+  </div>
+  `
+    )
+    .join("");
 }
 
+//CREATE ELEMENT E APPENDCHILD DE EXEMPLO
+const list = document.getElementById("list-container");
+function createElement() {
+  // Cria um novo elemento div
+  const newDiv = document.createElement("div");
+  // Adiciona conteúdo ao novo elemento div
+  newDiv.innerHTML =
+    "Este é um novo elemento div criado com JavaScript usando createElement e AppendChild";
+  // Adiciona o novo elemento div ao documento HTML
+  list.appendChild(newDiv);
+}
+
+createElement();
+
+//FUNÇÕES ANTIGAS
+//console.log(example, data);
+
+// //document.getElementById("list-container").innerHTML = showAll(data);
+
+// /*document.querySelector(".animation_cards").innerHTML = showInfo(data);
+
+// function showInfo(data) {
+//   const allAnimations = [];
+//   const allAnimationsPoster = [];
+//   for (let i = 0; i < data.films.length; i++) {
+//     const animationPoster = data.films[i].poster
+//     const animationTitle = data.films[i].title;
+//     const animationRelease = data.films[i].release_date;
+//     allAnimations.push(animationTitle + " - " + animationRelease);
+//   }
+//   return `<ul>
+//             ${allAnimations.map((item) => `<li>${item}</li>`).join("")}
+//           </ul>`;
+// }*/
+
+// function showInfo(data) {
+//   const allAnimations = [];
+//   const allAnimationsPosters = [];
+//   for (let i = 0; i < data.films.length; i++) {
+//     const animationPoster = data.films[i].poster;
+//     const animationTitle = data.films[i].title;
+//     const animationRelease = data.films[i].release_date;
+//     allAnimations.push(animationTitle + " - " + animationRelease);
+//     allAnimationsPosters.push(animationPoster);
+//   }
+//   return `<ul>
+//             ${allAnimations.map((item) => `<li>${item}</li>`).join("")}
+//           </ul>`;
+// }
