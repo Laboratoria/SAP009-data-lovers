b7ce7f94fee934957f6b8bfedd229d678601e24b
talitamsx <talitamsx@gmail.com>
2023-02-10 12:44:55 -0300

    header responsivo

--- src/main.js
+++ src/main.js
@@ -1,6 +1,51 @@
-import { example } from './data.js';
-// import data from './data/lol/lol.js';
+//import { example } from './data.js';
+
+const btnMenu = document.getElementById('toggleSidebar'); 
+const btnFechar = document.getElementById('closeSidebar');
+const header = document.getElementById('header');
+const navigationHeader = document.getElementById('navigation_header');
+const content = document.getElementById('content');
+let showSidebar = false;
+
+function toggleSidebar (){
+  showSidebar = !showSidebar;
+  if(showSidebar) {
+    navigationHeader.style.marginLeft = '-10vw'; 
+    navigationHeader.style.animationName = 'ShowSidebar'; 
+    content.style.filter = 'blur(2px)'; 
+  }
+  else{
+    navigationHeader.style.marginLeft = '-100vw';
+    navigationHeader.style.animationName = '';
+    content.style.filter = ''; 
+  }
+}
+btnMenu.addEventListener('click', toggleSidebar); 
+
+function closeSidebar(){
+  if (showSidebar){
+    toggleSidebar();
+  }
+}
+btnFechar.addEventListener('click', closeSidebar);
+content.addEventListener('click', closeSidebar);
+
+window.addEventListener('resize', function() {
+  if(window.innerWidth > 768 && showSidebar){
+    toggleSidebar();
+  }
+  
+});
+
+
+
+
+ 
+
+/*
+//import data from './data/lol/lol.js';
 import data from './data/pokemon/pokemon.js';
 // import data from './data/rickandmorty/rickandmorty.js';
 
 console.log(example, data);
+*/
