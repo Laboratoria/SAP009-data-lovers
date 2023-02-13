02af5e17e78583a9b609e836ff507e56e3d2889c
Thainara Tabile <thainara.tabilebenevides@gmail.com>
2023-02-13 13:58:29 -0300

    CSS responsivo

--- src/main.js
+++ src/main.js
@@ -1,21 +1,19 @@
 
 import lolData from "./data/lol/lol.js"
-import {allFilters }from "./data.js"
-console.log(allFilters);
+import { buscaTag, ordenarNomes, buscaNome, } from "./data.js"
 // pegando os dados do lol.js e salvando na var lolData
 
 const campeoes = Object.values(lolData.data);
 // essa const atribuiu valor dos objetos do lol.js, para acessá-los incluir o nome da var da linha 1.data (pq   estão dentro do atributo com nome data)
 function mostraCards(campeoes) {
-    document.getElementById('exibirCards').innerHTML = campeoes.map((campeao) => 
-    // aqui começa a interpolação da string
+    document.getElementById('exibirCards').innerHTML = campeoes.map((campeao) =>
+        // aqui começa a interpolação da string
         ` 
             <div class="cards">
                 <div class="card">
                     <div class="card-frente">
-                        <img src="${campeao.splash}" class="imagem-do-card">
-                        <h2 class="nome-do-card">${campeao.name.toUpperCase()}</h2>
-                        <p class="titulo-do-card">${campeao.title}</p>
+                    <img src="${campeao.splash}" class="imagem-do-card">
+                        <h2 id="nome-do-card">${campeao.name.toUpperCase()}</h2>
                     </div>
                     <div class="card-verso">
                     <h3 class="nome-verso">${campeao.name.toUpperCase()}</h3>
@@ -29,9 +27,35 @@ function mostraCards(campeoes) {
                     </div>
                 </div>
             </div>
-        `       
+        `
     ).join('')
 }
 
 window.addEventListener('load', () => mostraCards(campeoes));
 
+
+const liAssassino = document.querySelector(".li-assassino");
+liAssassino.addEventListener('click', function () {
+    const assassinos = buscaTag(campeoes, "Assassin");
+    mostraCards(assassinos);
+});
+
+// const buscaNome = document.querySelector("#txtBusca").value;
+// const btnBusca = document.querySelector("#btn-busca");
+// btnBusca.addEventListener('click', function(){
+// const resultado = ordenarNomes(buscaNome, parseInt);
+// document.querySelector("#exibirCards").innerHTML = resultado;
+
+// });
+
+const buscaCampeao = document.querySelector("#txt-busca");
+buscaCampeao.addEventListener("input", function () {
+    const nomeInput = buscaCampeao.value;
+    const arrayNome = buscaNome(campeoes, nomeInput);
+    mostraCards(arrayNome);
+});
+// testar charCode  ou return toUpperCase ou toLowerCase ou includes()
+
+// para pesquisar no console nome e poder console.log(campeoes[2].name + campeoes[2].info.attack)
+
+//- testar esse aqui se funciona na classificação, change ao invés de click
