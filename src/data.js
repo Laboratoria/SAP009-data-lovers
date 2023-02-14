7ebbd6977646e633337fdfc93e4c91a804347369
silvertapecoder <eusou_anacarolina@hotmail.com>
2023-02-14 17:43:52 -0300

    nada

--- src/data.js
+++ src/data.js
@@ -1,7 +1,16 @@
-export const example = () => {
-  return 'example';
-};
+/*const allInformations = JSON.parse("harry.json")
 
-export const anotherExample = () => {
-  return 'OMG';
-};
+console.log(allInformations);
+
+allInformations.map((characters)) => {
+  console.log(characters.name)
+}
+
+
+/*let personagens = document.querySelector(id do lugar onde vai ser o botao de "todos os personagens")
+
+fetch("harry.json").then(response) => {
+  response.json().then(characters) => {
+    console.log (characters);
+  }
+}*/
