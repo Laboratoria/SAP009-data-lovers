17b04d2228d6edd8a604f4ee648043c3ed7a4bec
Giselle Schwab Silva <giselleschwab@gmail.com>
2023-02-15 17:17:22 -0300

    print dos cards

--- src/main.js
+++ src/main.js
@@ -1,5 +1,24 @@
 import { example } from './data.js';
-import POKEMON from './data/pokemon/pokemon.js';
+// import data from './data/lol/lol.js';
+import data from './data/pokemon/pokemon.js';
+// import data from './data/rickandmorty/rickandmorty.js';
 
 
-console.log(example, POKEMON);
+const pokemons = data.pokemon;
+
+function mostrarPokemon(data) {
+  const mostrarPokemonTela = document.getElementById("inserirCards");
+  mostrarPokemonTela.innerHTML = data.map((item) =>
+    `
+    <div class = "cards-container">
+   <div class= "cards-container-frente">
+      <h2 class = "nome-pokemons">${item.name}</h2>
+      <img class ="img-estilo" src=${item.img}>
+      <p class = "tipos-pokemons"> ${item.type.join(" ")}</p>                   
+    </div>
+ </div>
+ `
+  ).join("")
+
+}
+mostrarPokemon(pokemons)
