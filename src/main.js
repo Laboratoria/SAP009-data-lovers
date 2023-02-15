08175fdec64d90fe7f10b4705ddd034551acb3d4
THALINascimento <thalia.secre@gmail.com>
2023-02-15 17:06:48 -0300

    alteração back

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
-import { example } from './data.js';
+import { example } from "./data.js";
 // import data from './data/lol/lol.js';
-import data from './data/pokemon/pokemon.js';
+//import data from './data/harrypotter/harry.json';
 // import data from './data/rickandmorty/rickandmorty.js';
 
+const personagens = Object.values(harry);
 console.log(example, data);
