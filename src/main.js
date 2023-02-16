b8bdbbee009a0a511d8ffc8b72f3d88eec182120
osikmelina <melina.mk@gmail.com>
2023-02-16 17:43:05 -0300

    encontrando os dados no js

--- src/main.js
+++ src/main.js
@@ -3,4 +3,15 @@ import { example } from './data.js';
 import data from './data/ghibli/ghibli.js';
 // import data from './data/rickandmorty/rickandmorty.js';
 
-console.log(example, data);
+const arrayGhibli = data;
+const filmes = arrayGhibli["filmes"]
+
+var cartaz = filmes.map(function(item){
+  return item.poster;
+})
+
+console.log(cartaz)
+
+const root = document.getElementById("root");
+
+root.innerHTML = `${cartaz}`
