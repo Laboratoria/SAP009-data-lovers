b00a94ad67aff5d0887348aa451032cce764e88a
Stellazen <stellazen.xd@gmail.com>
2023-02-18 14:32:04 -0300

    alterações quinta feira - 16/01

--- src/main.js
+++ src/main.js
@@ -1,6 +1,19 @@
 import { example } from './data.js';
-// import data from './data/lol/lol.js';
-import data from './data/ghibli/ghibli.js';
-// import data from './data/rickandmorty/rickandmorty.js';
 
-console.log(example, data);
+import data from './data/ghibli/ghibli.js';
+
+// console.log(example,data)
+
+const arrayGhibli = data;
+const filmes = arrayGhibli["filmes"]
+
+const cartaz = filmes.map(function(item){
+  return item.poster;
+})
+
+console.log(cartaz);
+
+const root = document.getElementById("root");
+
+root.innerHTML = `${cartaz}`
+
