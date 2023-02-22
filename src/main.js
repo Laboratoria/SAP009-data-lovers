04f46b390853d0adfb34d3179f21c33ef795bfca
osikmelina <melina.mk@gmail.com>
2023-02-22 12:38:52 -0300

    posteres adicionados na página pelo JS

--- src/main.js
+++ src/main.js
@@ -4,14 +4,20 @@ import data from './data/ghibli/ghibli.js';
 // import data from './data/rickandmorty/rickandmorty.js';
 
 const arrayGhibli = data;
-const filmes = arrayGhibli["filmes"]
-
-var cartaz = filmes.map(function(item){
-  return item.poster;
-})
-
-console.log(cartaz)
-
+const filmes = arrayGhibli["films"]
 const root = document.getElementById("root");
+let posteres = ''
 
-root.innerHTML = `${cartaz}`
+filmes.map(function(item) {
+  posteres += `<div class="poster"><img src="${item.poster}"></div>`;  // mapeando o array filmes e devolve na let posteres todos os itens com nome poster dentro do arquivo ghibli.js
+})
+root.innerHTML = posteres  // avisa o JS que isso vai pro HTML
+
+filmes.sort(function(a,b) {
+  if (a.tittle < b.tittle) {
+    return -1
+  }
+  if (b.tittle < a.tittle) {
+    return 1
+  }
+})
