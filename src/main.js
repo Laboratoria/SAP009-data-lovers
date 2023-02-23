2b5b82c66e3bd2a63701e5b78eec362be30d3c42
osikmelina <melina.mk@gmail.com>
2023-02-23 10:11:39 -0300

    função ordenar de A a Z e ano descrescente ok

--- src/main.js
+++ src/main.js
@@ -13,11 +13,32 @@ filmes.map(function(item) {
 })
 root.innerHTML = posteres  // avisa o JS que isso vai pro HTML
 
-filmes.sort(function(a,b) {
-  if (a.tittle < b.tittle) {
-    return -1
+const seletorDeOrdem = document.getElementById("ordem")
+seletorDeOrdem.addEventListener('change', function(event){
+  let posteresOrdenados = ''
+
+  if(event.target.value === 'deAZ') {
+    const deAZ = data.films.sort((a,b) => a.title.localeCompare(b.title))
+    deAZ.map(function(item) {
+      posteresOrdenados += `<div class="poster"><img src="${item.poster}"></div>`;  // mapeando o array filmes e devolve na let posteres todos os itens com nome poster dentro do arquivo ghibli.js
+    })
   }
-  if (b.tittle < a.tittle) {
-    return 1
+
+  if(event.target.value === 'deZA') {
+    console.log('deZA')
   }
+
+  if(event.target.value === 'crescente') {
+    console.log('crescente')
+  }
+
+  if(event.target.value === 'decrescente') {
+    const decrescente = data.films.sort((a,b) => b.release_date.localeCompare(a.release_date));
+
+    decrescente.map(function(item) {
+      posteresOrdenados += `<div class="poster"><img src="${item.poster}"></div>`;  // mapeando o array filmes e devolve na let posteres todos os itens com nome poster dentro do arquivo ghibli.js
+    })
+  }
+
+  root.innerHTML = posteresOrdenados
 })
