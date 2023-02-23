da5f403f0f2d85da8e2e2b2ab8acf6c35007579f
euerika <euerika.soaresdossantos@gmail.com>
2023-02-22 21:03:23 -0300

    Card Tela Inicial

--- src/main.js
+++ src/main.js
@@ -1,6 +1,28 @@
-import { example } from './data.js';
-// import data from './data/lol/lol.js';
-import data from './data/pokemon/pokemon.js';
-// import data from './data/rickandmorty/rickandmorty.js';
+import {} from "./data.js";
 
-console.log(example, data);
+import data from "./data/harrypotter/harry.js";
+
+const animationCards = document.querySelector(".animation-cards");
+
+const linkPersonagens = document.getElementById("listaPerso");
+linkPersonagens.addEventListener("click", teste);
+const allPersonagens = data.characters;
+
+function teste() { 
+
+  const animationCardsHTML = allPersonagens
+    .map((element, index) => {
+      return `      
+        <div class="cards">     
+          <p id="film-title" class="film-info">${element.name}</p>
+          <p class="film-info">${element.books_featured_in}</p>
+          <button class="more" id="${index}">More</button>
+        </div>
+      `;
+    })
+    .join("");
+    
+
+  animationCards.innerHTML = animationCardsHTML;
+}
+// showAnimations(personagens);
