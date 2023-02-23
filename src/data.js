63d53cd0de8ea6607aef89cdc1255fe183434a09
Polyana <polyanacristina2001@gmail.com>
2023-02-23 14:47:09 -0300

    interações e estilização

--- src/data.js
+++ src/data.js
@@ -1,9 +1,9 @@
-// estas funciones son de ejemplo
-
-export const example = () => {
-  return 'example';
-};
-
-export const anotherExample = () => {
-  return 'OMG';
+import data from './data/harrypotter/data.js';
+export const filtrarPersonagens = (valor) => {
+  return data.characters.filter((personagem) => {
+    if (personagem.name.toLocaleLowerCase().startsWith(valor.toLocaleLowerCase())) {
+      return true
+    }
+    return false
+  } )
 };
