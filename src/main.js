ef100b9e94dee67b784233592a0c8b431910f996
Stellazen <stellazen.xd@gmail.com>
2023-02-23 09:55:36 -0300

    teste

--- src/main.js
+++ src/main.js
@@ -10,34 +10,18 @@ const root = document.getElementById("root");
 let posteres = ''
 
 filmes.map(function(item) {
-  posteres += `<div class="poster"><img src="${item.poster}"></div>`;  // mapeando o array filmes e devolve na let posteres todos os itens com nome poster dentro do arquivo ghibli.js
+  posteres += `<img src="${item.poster}">`;  // mapeando o array filmes e devolve na let posteres todos os itens com nome poster dentro do arquivo ghibli.js
 })
 root.innerHTML = posteres  // avisa o JS que isso vai pro HTML
 
-// const alfabeto = filmes.sort(function(a,b){
-//   if(a.title < b.title){
-//     return -1
-//   }
-//   if(b.title < a.title){
-//     return 1
-//   }
-// })
-// console.log(alfabeto)
-
 const alfabeto = data.films.sort((a,b) => a.title.localeCompare(b.title));
 
+const score = data.films.sort((a,b) => Number(a.rt_score) - (Number(b.rt_score)));
 
-const ano = filmes.sort(function(a,b){
-  if(a.release_date < b.release_date){
-    return -1
-  }
-  if(b.release_date < a.release_date){
-    return 1
-  }
-})
+//const anoDecrescente = document.getElementById("decrescente")
 
-const anoDecrescente = document.getElementById("decrescente")
-anoDecrescente.onclick = function(){
-  data.release_date.sort((a,b) => b.release_date.localeCompare(a.release_date));
-}
-console.log(anoDecrescente)
+//const ordemD = data.films.sort((a,b) => b.release_date.localeCompare(a.release_date));
+
+console.log(alfabeto)
+//console.log(ordemD)
+console.log(score)
