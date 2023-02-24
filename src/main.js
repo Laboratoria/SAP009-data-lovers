c188ec14f64210ce8dcd0f11416673b76f5a2072
TataSchultz <tarcila.schultz@gmail.com>
2023-02-24 19:06:17 -0300

    alterar main.js e index.html

--- src/main.js
+++ src/main.js
@@ -1,31 +1,28 @@
-// // import { example } from './data.js';
-// // // import data from './data/lol/lol.js';
- import data from './data/lol/lol.js';
+import data from "./data/lol/lol.js";
+//import {example} from './data.js';
 
-const allChampions = data.data; // aqui tem um objeto com todas as informaçoes dos camps. 
+const allChampions = data.data;
+const interactionCards = document.querySelector(".animation-cards");
+const inputChampions = document.getElementById("search");
+inputChampions.addEventListener("click", teste);
+//const allPersonagens = data.characters;
 
-//a função mostra o objeto, com objetos dentro 
-function showAnimation (allChampions) {
-    console.log(allChampions);
+function teste() { 
+  const championsArray = Object.values(allChampions);
+  return championsArray
+    .map((champion, index) => 
+      ` 
+      <div class="card">
+      <img id="${index}" class="posters" src="${champion.splash}" alt="Pôster de">
+      <p id="film-title" class="film-info">${champion.title} </p>
+      <p class="film-info"> ${champion.stats.armorperlevel}</p>
+      <button class="more" id="${index}">More</button>
+      </div>
+        
+        </div>  
+      `
+    )
+    .join("");
+  interactionCards.innerHTML = championsArray
 
-// converte o objeto em array 
-const championsArray = Object.values (allChampions);
-// mostra a array convertida, acessada pelos indices de (0 a 134)
-console.log(championsArray);
-// acessa o item 11 da array e exibe o id, ou seja, nome do campeao 
-console.log(championsArray[11].id);
-//metodo map- para mapear a array 
-return championsArray
- .map(
-    //champion é cada personagem 
-    //index é o número deles na tela
-    (champion, index) =>
-    <img id= "${index}" class ="posters" src="$ {champion.splash}" alt= "poster dos campeões">
-        < p id= "championNameCard" class= "champion-name"> ${champion.id} </p>
-        < p class="champion-name"> ${champion.tags}</p>
-        <button class="more" id="${index}"> Mais iformação </button>
-        </div>
- )
-  .join("");
- } 
-    
+}
