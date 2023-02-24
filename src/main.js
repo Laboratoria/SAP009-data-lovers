58211bfbd458538d0137d2af4387d08cb84be5b2
Stellazen <stellazen.xd@gmail.com>
2023-02-24 14:39:30 -0300

    oh Sebastian-forEach

--- src/main.js
+++ src/main.js
@@ -21,32 +21,32 @@ seletorDeOrdem.addEventListener('change', function (event) {
   if (event.target.value === 'deAZ') {
     const deAZ = data.films.sort((a, b) => a.title.localeCompare(b.title))
     deAZ.map(function (item) {
-      posteresOrdenados += `<img src="${item.poster}">`;  // mapeando o array filmes e devolve na let posteres todos os itens com nome poster dentro do arquivo ghibli.js
+      posteresOrdenados += `<img class="filme" src="${item.poster}">`;  // mapeando o array filmes e devolve na let posteres todos os itens com nome poster dentro do arquivo ghibli.js
     })
   }
 
   if (event.target.value === 'deZA') {
     const deZA = data.films.sort((a, b) => b.title.localeCompare(a.title))
     deZA.map(function (item) {
-      posteresOrdenados += `<img src="${item.poster}">`;
+      posteresOrdenados += `<img class="filme" src="${item.poster}">`;
     })
   }
 
   if (event.target.value === 'crescente') {
     const crescente = data.films.sort((a, b) => a.release_date.localeCompare(b.release_date.localeCompare))
     crescente.map(function (item) {
-      posteresOrdenados += `<img src="${item.poster}">`;
+      posteresOrdenados += `<img class="filme" src="${item.poster}">`;
     })
   }
 
   if (event.target.value === 'decrescente') {
     const decrescente = data.films.sort((a, b) => b.release_date.localeCompare(a.release_date.localeCompare));
     decrescente.map(function (item) {
-      posteresOrdenados += `<img src="${item.poster}">`;  // mapeando o array filmes e devolve na let posteres todos os itens com nome poster dentro do arquivo ghibli.js
+      posteresOrdenados += `<img class="filme" src="${item.poster}">`;  // mapeando o array filmes e devolve na let posteres todos os itens com nome poster dentro do arquivo ghibli.js
     })
   }
 
-  if (event.target.value === 'rt_score') {
+  if (event.target.value === 'score') {
     const score = data.films.sort(function (a, b) {
       if (Number(a.rt_score) < Number(b.rt_score)) {
         return 1;
@@ -54,14 +54,19 @@ seletorDeOrdem.addEventListener('change', function (event) {
       if (Number(b.rt_score) < Number(a.rt_score)) {
         return -1;
       }
-      
-      score.map(function(item){
-        posteresOrdenados += `<img src="${item.poster}">`;
-      })
-      
-    })
+    }) 
+    score.map(function(item){
+      posteresOrdenados += `<img class="filme" src="${item.poster}">`;
+    })  
   }
   root.innerHTML = posteresOrdenados
+
+  let filmes = document.querySelectorAll(".filme")
+  console.log(filmes)
+  filmes.forEach((filme)=> filme.addEventListener("click",(event)=>{
+  console.log(filme)
+  }
+  ))
 })
 
 
