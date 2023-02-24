2171ad660d28a002a129ce99dc1a8c1b67f1766f
Stellazen <stellazen.xd@gmail.com>
2023-02-24 16:43:46 -0300

    inicio modal

--- src/main.js
+++ src/main.js
@@ -61,16 +61,24 @@ seletorDeOrdem.addEventListener('change', function (event) {
   }
   root.innerHTML = posteresOrdenados
 
-  let filmes = document.querySelectorAll(".filme")
-  console.log(filmes)
-  filmes.forEach((filme)=> filme.addEventListener("click",(event)=>{
-  console.log(filme)
+  const todosOsFilmes = document.querySelectorAll(".filme")
+  console.log(todosOsFilmes)
+  todosOsFilmes.forEach((filme)=> filme.addEventListener("click",(event)=>{
+    console.log(filme)
   }
   ))
 })
 
+//teste modal-dialog:
+const abrir = document.querySelector(".abrir");
+const fechar = document.querySelector(".fechar")
+const bloco = document.getElementById("bloco")
 
-// const score = data.films.sort((a,b) => Number(a.rt_score) - (Number(b.rt_score)));
-// score.map(function(item){
-// })
-//console.log(score)
+abrir.addEventListener("click",function(){
+  bloco.showModal();
+})
+
+fechar.addEventListener("click",function(){
+  bloco.close();
+})
+
