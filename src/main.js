57ce5cccd93d27c861711d7ffd8d05763836607f
euerika <euerika.soaresdossantos@gmail.com>
2023-02-23 22:21:57 -0300

    Estilo do Card para o primeiro filtro

--- src/main.js
+++ src/main.js
@@ -9,14 +9,12 @@ linkPersonagens.addEventListener("click", teste);
 const allPersonagens = data.characters;
 
 function teste() { 
-
   const animationCardsHTML = allPersonagens
     .map((element, index) => {
       return `      
-        <div class="cards">     
+        <div class="cards">         
           <p id="film-title" class="film-info">${element.name}</p>
-          <p class="film-info">${element.books_featured_in}</p>
-          <button class="more" id="${index}">More</button>
+          <p class="film-info">${element.species}</p>
         </div>
       `;
     })
@@ -25,23 +23,60 @@ function teste() {
 }
 
 const linkLivros = document.getElementById("listaLivros");
-linkLivros.addEventListener("click", teste);
+linkLivros.addEventListener("click", testeBook);
 const allBooks = data.books;
-console.log(allBooks)
 
-// function teste() { 
+function testeBook() { 
 
-//   const animationCardsHTML = allBooks
-//     .map((element, index) => {
-//       return `      
-//         <div class="cards">     
-//           <p id="film-title" class="film-info">${element.author}</p>
-//           <p class="film-info">${element.books_featured_in}</p>
-//           <button class="more" id="${index}">More</button>
-//         </div>
-//       `;
-//     })
-//     .join("");
-//   animationCards.innerHTML = animationCardsHTML;
-// }
+  const animationCardsHTML = allBooks
+    .map((element, index) => {
+      return `      
+        <div class="cards">     
+          <p id="film-title" class="film-info">${element.title}</p>
+          <p class="film-info">${element.description}</p>
+          <button class="more" id="${index}">More</button>
+        </div>
+      `;
+    })
+    .join("");
+  animationCards.innerHTML = animationCardsHTML;
+}
+const linkFeitiços = document.getElementById("listaFeitiços");
+linkFeitiços.addEventListener("click", testeFeitiços);
+const allSpells = data.spells;
+
+function testeFeitiços() { 
+
+  const animationCardsHTML = allSpells
+    .map((element, index) => {
+      return `      
+        <div class="cards">     
+          <p id="film-title" class="film-info">${element.name}</p>
+          <p class="film-info">${element.pronunciation}</p>
+          <button class="more" id="${index}">More</button>
+        </div>
+      `;
+    })
+    .join("");
+  animationCards.innerHTML = animationCardsHTML;
+}
+const linkCuriosidades = document.getElementById("listaCuriosidades");
+linkCuriosidades.addEventListener("click", testeCuriosidades);
+const allCuriosidades = data.funFacts;
+
+function testeCuriosidades() { 
+
+  const animationCardsHTML = allCuriosidades
+    .map((element, index) => {
+      return `      
+        <div class="cards">     
+          <p id="film-title" class="film-info">${element.type}</p>
+          <p class="film-info">${element.content}</p>
+          <button class="more" id="${index}">More</button>
+        </div>
+      `;
+    })
+    .join("");
+  animationCards.innerHTML = animationCardsHTML;
+}
 // showAnimations(personagens);
