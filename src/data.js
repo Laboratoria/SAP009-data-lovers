c28b91a78ba811ee10b4f633d04ca86bc27be58c
osikmelina <melina.mk@gmail.com>
2023-02-24 16:42:17 -0300

    passando as funç

--- src/data.js
+++ src/data.js
@@ -1,7 +1,44 @@
-// estas funciones son de ejemplo
+import data from './data/ghibli/ghibli.js';
 
-export const example = () => {
-  return 'example';
+export const ordenar = (ordem) => {
+  let posteresOrdenados = ''
+
+  if(ordem === 'deAZ') {
+    const deAZ = data.films.sort((a,b) => a.title.localeCompare(b.title))
+    deAZ.map(function(item) {
+      posteresOrdenados += `<div class="poster"><img src="${item.poster}"></div>`;  // mapeando o array filmes e devolve na let posteres todos os itens com nome poster dentro do arquivo ghibli.js
+    })
+  }
+
+  if(ordem === 'deZA') {
+    const deZA = data.films.sort((a,b) => b.title.localeCompare(a.title))
+    deZA.map(function(item) {
+      posteresOrdenados += `<div class="poster"><img src="${item.poster}"></div>`;
+    })
+  }
+
+  if(ordem === 'crescente') {
+    const crescente = data.films.sort((a,b) => a.release_date.localeCompare(b.release_date));
+    crescente.map(function(item) {
+      posteresOrdenados += `<div class="poster"><img src="${item.poster}"></div>`;
+    })
+  }
+
+  if(ordem === 'decrescente') {
+    const decrescente = data.films.sort((a,b) => b.release_date.localeCompare(a.release_date));
+    decrescente.map(function(item) {
+      posteresOrdenados += `<div class="poster"><img src="${item.poster}"></div>`;  // mapeando o array filmes e devolve na let posteres todos os itens com nome poster dentro do arquivo ghibli.js
+    })
+  }
+
+  if(ordem === 'score') {
+    const score = data.films.sort((a,b) => Number(a.rt_score) - Number(b.rt_score));
+    score.map(function(item) {
+      posteresOrdenados += `<div class="poster"><img src="${item.poster}"></div>`;
+    })
+  }
+  
+  return posteresOrdenados
 };
 
 export const anotherExample = () => {
--- src/main.js
+++ src/main.js
@@ -1,8 +1,6 @@
-import { example } from './data.js';
+import { ordenar } from './data.js';
 
 import data from './data/ghibli/ghibli.js';
-
-// console.log(example,data)
 
 const arrayGhibli = data;
 const filmes = arrayGhibli["films"]
@@ -16,42 +14,7 @@ root.innerHTML = posteres  // avisa o JS que isso vai pro HTML
 
 const seletorDeOrdem = document.getElementById("ordem")
 seletorDeOrdem.addEventListener('change', function(event){
-  let posteresOrdenados = ''
-
-  if(event.target.value === 'deAZ') {
-    const deAZ = data.films.sort((a,b) => a.title.localeCompare(b.title))
-    deAZ.map(function(item) {
-      posteresOrdenados += `<div class="poster"><img src="${item.poster}"></div>`;  // mapeando o array filmes e devolve na let posteres todos os itens com nome poster dentro do arquivo ghibli.js
-    })
-  }
-
-  if(event.target.value === 'deZA') {
-    const deZA = data.films.sort((a,b) => b.title.localeCompare(a.title))
-    deZA.map(function(item) {
-      posteresOrdenados += `<div class="poster"><img src="${item.poster}"></div>`;
-    })
-  }
-
-  if(event.target.value === 'crescente') {
-    const crescente = data.films.sort((a,b) => a.release_date.localeCompare(b.release_date));
-    crescente.map(function(item) {
-      posteresOrdenados += `<div class="poster"><img src="${item.poster}"></div>`;
-    })
-  }
-
-  if(event.target.value === 'decrescente') {
-    const decrescente = data.films.sort((a,b) => b.release_date.localeCompare(a.release_date));
-    decrescente.map(function(item) {
-      posteresOrdenados += `<div class="poster"><img src="${item.poster}"></div>`;  // mapeando o array filmes e devolve na let posteres todos os itens com nome poster dentro do arquivo ghibli.js
-    })
-  }
-
-  if(event.target.value === 'score') {
-    const score = data.films.sort((a,b) => Number(a.rt_score) - Number(b.rt_score));
-    score.map(function(item) {
-      posteresOrdenados += `<div class="poster"><img src="${item.poster}"></div>`;
-    })
-  }
+  const posteresOrdenados = ordenar(event.target.value)
 
   root.innerHTML = posteresOrdenados
 })
