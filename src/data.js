f78f31c16a5954db8a57e7ac7f1e9f8dd0effd76
euerika <euerika.soaresdossantos@gmail.com>
2023-02-25 21:16:07 -0300

    Filtro na Tela

--- src/data.js
+++ src/data.js
@@ -1,3 +1,11 @@
+export function filtrarCasa(listArray, valueSelect) {
+  const casasPersonagem = listArray.filter((itemListaArray) =>
+    itemListaArray.house.includes(valueSelect)
+  );
+  //filtro precisa filtrar apenas o item indicado
+  //includes retornar verdadeiro ou falso caso encontre o que está procurando
 
+  return casasPersonagem;
+}
 
 
