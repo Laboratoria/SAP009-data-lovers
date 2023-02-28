bf155bef847b67c9da570f196dcbf8e62aa32626
marinacezario <marina.cezario@gmail.com>
2023-02-28 14:24:52 -0300

    indentação e botão

--- src/main.js
+++ src/main.js
@@ -30,19 +30,36 @@ function mostrarPokemon(data) {
           <div class = "cards-container-posterior">
             <div class = "extra-info2"> <p class="letras"> <span>Altura </span>${item.size.height}</p></div>
             <div class = "extra-info2"> <p class="letras"> <span>Peso </span>${item.size.weight}</p></div>
-            <div class = "extra-info2"> <p class="letras"> <span>Evolução </span>${item.candycost}</p></div>
             <div class = "extra-info2"> <p class="letras"> <span>Resistências </span>${item.resistant.join(", ")}</p></div>
             <div class = "extra-info2"> <p class="letras"> <span>Fraquezas </span>${item.weaknesses.join(", ")}</p></div>
           </div> 
         </div>  
       </div>  
-  
  `
   ).join("")
 
 }
 mostrarPokemon (pokemons)
 
+// Pegar o botão
+const meuBotao = document.getElementById("myBtn");
 
+// Quando usuário desce 20px do topo do documento, mostraro botão
+window.onscroll = function() {scrollFuncao()};
+
+function scrollFuncao() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    meuBotao.style.display = "block";
+  } else {
+    meuBotao.style.display = "none";
+  }
+}
+
+// Quando o usuário clica no botao, rola para o topo do documento
+
+function topFuncao() {
+  document.body.scrollTop = 0; // Safari
+  document.documentElement.scrollTop = 0; // Chrome, Firefox, IE e Opera
+}
 
 
