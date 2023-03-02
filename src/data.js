99ad70d2a51b05ed492883aacfd73389995006ff
osikmelina <melina.mk@gmail.com>
2023-03-02 13:04:06 -0300

    funções organizadas com otimização de dados entre main e data.js

--- src/data.js
+++ src/data.js
@@ -1,50 +1,35 @@
-import data from './data/ghibli/ghibli.js';
-
-export const ordenar = (ordem) => {
-  let posteresOrdenados = ''
+export const ordenar = (films, ordem) => {
 
   if(ordem === 'deAZ') {
-    const deAZ = data.films.sort((a,b) => a.title.localeCompare(b.title))
-    deAZ.map(function(item) {
-      posteresOrdenados += `<img class="filme" src="${item.poster}" data-id="${item.id}">`;  // mapeando o array filmes e devolve na let posteres todos os itens com nome poster dentro do arquivo ghibli.js
-    })
+    return films.sort((a,b) => a.title.localeCompare(b.title))
+
   }
 
   if(ordem === 'deZA') {
-    const deZA = data.films.sort((a,b) => b.title.localeCompare(a.title))
-    deZA.map(function(item) {
-      posteresOrdenados += `<img class="filme" src="${item.poster}" data-id="${item.id}">`;
-    })
+    return films.sort((a,b) => b.title.localeCompare(a.title))
+   
   }
 
   if(ordem === 'crescente') {
-    const crescente = data.films.sort((a,b) => a.release_date.localeCompare(b.release_date));
-    crescente.map(function(item) {
-      posteresOrdenados += `<img class="filme" src="${item.poster}" data-id="${item.id}">`;
-    })
+    return films.sort((a,b) => a.release_date.localeCompare(b.release_date));
+    
   }
 
   if(ordem === 'decrescente') {
-    const decrescente = data.films.sort((a,b) => b.release_date.localeCompare(a.release_date));
-    decrescente.map(function(item) {
-      posteresOrdenados += `<img class="filme" src="${item.poster}" data-id="${item.id}">`;  // mapeando o array filmes e devolve na let posteres todos os itens com nome poster dentro do arquivo ghibli.js
-    })
+    return films.sort((a,b) => b.release_date.localeCompare(a.release_date));
+  
   }
 
   if(ordem === 'score') {
-    const score = data.films.sort((a,b) => Number(b.rt_score) - Number(a.rt_score));
-    score.map(function(item) {
-      posteresOrdenados += `<img class="filme" src="${item.poster}" data-id="${item.id}">`;
-    })
+    return films.sort((a,b) => Number(b.rt_score) - Number(a.rt_score));
+   
   }
-  
-  return posteresOrdenados
 };
 
-export const preencherModal = (posterClicado) => {
+export const preencherModal = (filmes, posterClicado) => {
   const idDoFilmeClicado = posterClicado.getAttribute('data-id')  // pegando o id do poster (que está num data-attribute) para identificar exatamente qual é o filme
 
-  const [informacoesDoFilmeCLicado] = data.films.filter((filme) => filme.id === idDoFilmeClicado)
+  const [informacoesDoFilmeCLicado] = filmes.filter((filme) => filme.id === idDoFilmeClicado)
 
   let personagens = ''
   informacoesDoFilmeCLicado.people.forEach((personagem, index) => {
@@ -64,17 +49,11 @@ export const preencherModal = (posterClicado) => {
   <p><strong>Personagens:</strong> ${personagens}</p>`
 }
 
-// export const exemplo = () => {
-//  return 'algo'
-// }
-
-export function resultadoDoFiltro (nome) { //'Hayao Miyazaki'
-  const filmesFiltradosPorProdutor = data.films.filter((item) => filtrarPorProdutor(item, nome))
+export function resultadoDoFiltro (filmes, nome) {
+  const filmesFiltradosPorProdutor = filmes.filter((item) => filtrarPorProdutor(item, nome))
   return filmesFiltradosPorProdutor
 }
 
 function filtrarPorProdutor(item, nome) {
   return item.producer === nome
 }
-
-resultadoDoFiltro('Hayao Miyazaki')
--- src/main.js
+++ src/main.js
@@ -3,14 +3,16 @@ import { ordenar, preencherModal, resultadoDoFiltro } from './data.js';
 import data from './data/ghibli/ghibli.js';
 
 const arrayGhibli = data;
-const filmes = arrayGhibli["films"]
+const filmes = arrayGhibli.films
 const root = document.getElementById("root");
-let posteres = ''
 
-filmes.map(function (item) {
-  posteres += `<img class="filme" src="${item.poster}" data-id="${item.id}">`;  // mapeando o array filmes e devolve na let posteres todos os itens com nome poster dentro do arquivo ghibli.js
-})
-root.innerHTML = posteres  // avisa o JS que isso vai pro HTML
+function renderizaPoster (posteres){
+  const cards = posteres.map(function (item) {
+    return `<img class="filme" src="${item.poster}" data-id="${item.id}">`;  // mapeando o array filmes e retornando todos os itens com nome poster e id
+  })
+  root.innerHTML = cards.join('') // avisa o JS que os posteres vao pro HTML - .join junta todos os itens do array pra que nao printe na tela com virgula, e a string vazia serve pra que nao fiquem colados um no outro
+}
+renderizaPoster(filmes)
 
 const todosPosteresDoHtml = document.querySelectorAll(".filme")  // selecionando todos os posteres e colocando dentro da variável
 todosPosteresDoHtml.forEach((umPosterDoHtml) => umPosterDoHtml.addEventListener("click",(event)=>{  // fazendo um loop nessa seleção de todos os posteres e adicionando o evento de clique para cada poster do html
@@ -23,23 +25,18 @@ todosPosteresDoHtml.forEach((umPosterDoHtml) => umPosterDoHtml.addEventListener(
 
 const seletorDeFiltro = document.getElementById("filtroProdutor")
 seletorDeFiltro.addEventListener('change', function(event){
-  let posteresFiltradosPorProdutor = ''
-  const filmesFiltrados = resultadoDoFiltro(event.target.value)
-  filmesFiltrados.map(function(item) {
-    posteresFiltradosPorProdutor += `<img class="filme" src="${item.poster}" data-id"${item.id}">`
-  })
-  root.innerHTML = posteresFiltradosPorProdutor
+  const filmesFiltrados = resultadoDoFiltro(filmes, event.target.value)
+  renderizaPoster(filmesFiltrados)
 })
 
 const seletorDeOrdem = document.getElementById("ordem")
 seletorDeOrdem.addEventListener('change', function(event){
-  const posteresOrdenados = ordenar(event.target.value)
-  
-  root.innerHTML = posteresOrdenados
+  const posteresOrdenados = ordenar(filmes, event.target.value)
+  renderizaPoster(posteresOrdenados)
 
   const todosPosteresDoHtml = document.querySelectorAll(".filme")
   todosPosteresDoHtml.forEach((umPosterDoHtml)=> umPosterDoHtml.addEventListener("click",(event)=>{
-    informacoes.innerHTML = preencherModal(event.target)
+    informacoes.innerHTML = preencherModal(filmes, event.target)
     // event.target -> identifica em que poster aconteceu o clique 
     // preencherModal -> chamando a função preencherModal e enviando o parametro event.target 
   
