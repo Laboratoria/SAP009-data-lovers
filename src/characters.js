002ae600066499e6c618fa588ac7e0b3162f8a90
IzadoraNascimento <izasnascimento@icloud.com>
2023-03-03 12:05:02 -0300

    janela modal funcionando com as informações dos personagens

--- src/characters.js
+++ src/characters.js
@@ -1,21 +1,22 @@
 import {films} from './data.js';
 import data from './data/ghibli/ghibli.js';
 
-
+// Função para imprimir os cards na tela
 function sectionCharacters(persona){
   document.getElementById('infoCharacters').innerHTML = persona.map((studio) => 
-  studio.people.map(film =>`
+  studio.people.map(personagem =>`
   <div class="cardFront">
-  <p class="name">${film.name}</p>
-  <img class="foto" src="${film.img}" alt="Poster do Personagem">
+  <p class="name">${personagem.name}</p>
+  <img data-id="${personagem.id}" class="foto" src="${personagem.img}" alt="Poster do Personagem">
   `).join("") 
 )}
 sectionCharacters(data.films)
 
 
 // Função para janela modal
-function modalPersona(modalID, data){
-  console.log(modalID)
+function modalPersona(modalID, personagemID){
+  /*console.log(personagemID)
+  console.log(modalID)*/
   const modal = document.getElementById(modalID);
   modal.classList.add('showModal')
   modal.addEventListener('click', (evento) => {
@@ -23,30 +24,35 @@ function modalPersona(modalID, data){
       modal.classList.remove('showModal');
     }
   })
+  
+  let personagens = []
+  data.films.forEach(filme => {
+    const personagensFiltrados = filme.people.filter((personagemAtual) => {
+      return personagemAtual.id == personagemID
+    })
+    personagens = [...personagens, ... personagensFiltrados]
+  })
 
-  const teste = document.getElementById('teste')
-  teste.innerHTML = `teste`
+  const personagem = personagens[0]
+
+  const modalContent = modal.querySelector('.modal-content')
+  console.log(personagem)
+
+  modalContent.innerHTML = `
+    <h1>${personagem.name}</h1>
+      <img class="poster" src="${personagem.img}">
+          <h3>Age:${personagem.age}</h3>
+          <h3>Gender: ${personagem.gender}</h3>
+          <h3> Specie: ${personagem.specie}</h3>
+          <h3>Hair Color: ${personagem.hair_color}</h3>
+          <h3>Eye Color: ${personagem.eye_color}</h3>
+  `;
 }
 
 const image = document.querySelector('.cardFront');
-image.addEventListener('click', function() {
-  modalPersona('modalChar');
+image.addEventListener('click', function(e) {
+  modalPersona('modalChar', e.target.dataset.id);
 })
 
-/*const modalData = document.querySelector(".modal")
+/*const modalData = document.querySelector(".modal")*/
 
-data.films.filter(itemAtual => {
-    return itemAtual.people.filter((personagemAtual) => {
-      return personagemAtual.id == modalPersona.id
-    })
-  })
-
-  modalData.innerHTML = `
-    <h1>${modalPersona.name}</h1>
-      <img class="poster" src="${modalPersona.img}">
-          <h3>Age:${modalPersona.age}</h3>
-          <h3>Gender: ${modalPersona.gender}</h3>
-          <h3> Specie: ${modalPersona.specie}</h3>
-          <h3>Hair Color: ${modalPersona.hair_color}</h3>
-          <h3>Eye Color: ${modalPersona.eye_color}</h3>
-  `;*/
