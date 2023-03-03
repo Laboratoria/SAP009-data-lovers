0bcdccf857f3a705ea43819c3bb6d4bc3419bc8e
IzadoraNascimento <izasnascimento@icloud.com>
2023-03-03 13:41:10 -0300

    janela modal personagens dados ok

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
-import {films} from './data.js';
+//import {films} from './data.js';
 import data from './data/ghibli/ghibli.js';
 
-
+// Função dos cards na tela
 function sectionMovies(items){
   document.getElementById('infoMovies').innerHTML = items.map((studio) => `
   <div class="cardFront">
@@ -11,7 +11,10 @@ function sectionMovies(items){
 }
 sectionMovies(data.films)
 
-function modalID(modalID){
+// Função da janela modal
+function modalFilmes(modalID, filmeID){
+  /*console.log(personagemID)
+  console.log(modalID)*/
   const modal = document.getElementById(modalID);
   modal.classList.add('showModal')
   modal.addEventListener('click', (evento) => {
@@ -19,11 +22,33 @@ function modalID(modalID){
       modal.classList.remove('showModal');
     }
   })
+  
+  let filmes = []
+  data.films.map(filme => {
+    const filmesFiltrados = filme.map((filmeAtual) => {
+      return filmeAtual.id == filmeID
+    })
+    
+    filmes = [...filmes, ... filmesFiltrados]
+  })
+
+  const movies = filmes[0]
+
+  const modalContent = modal.querySelector('.modal-content')
+  console.log(movies)
+
+  modalContent.innerHTML = `
+    <h1>${movies.title}</h1>
+          <h3>Description:${movies.description}</h3>
+          <h3>Director: ${movies.director}</h3>
+          <h3>Producer: ${movies.producer}</h3>
+          <h3>Date: ${movies.release_date}</h3>
+  `;
 }
 
 const image = document.querySelector('.cardFront');
-image.addEventListener('click', function() {
-  modalID('modalChar');
+image.addEventListener('click', function(e) {
+  modalFilmes('modalChar', e.target.dataset.id);
 })
 
 
