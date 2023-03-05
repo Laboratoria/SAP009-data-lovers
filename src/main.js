dc773ef60273550cacc2015180edf3549eec120b
Edimara Arcanjo <edimaraarcanjo21@gmail.com>
2023-03-05 15:13:15 -0300

    mostrando personagens

--- src/main.js
+++ src/main.js
@@ -1,6 +1,19 @@
-import { example } from './data.js';
-// import data from './data/lol/lol.js';
-import data from './data/pokemon/pokemon.js';
-// import data from './data/rickandmorty/rickandmorty.js';
+import data from './data/ghibli/ghibli.js';
 
-console.log(example, data);
+const mostrarCard = document.querySelector(".cards")
+
+// function showPeople() {
+// mostrarCard.innerHTML = ""
+// for (let i=0; i < data.films.length ; i++) {
+// for (let j = 0; j <data.films[i].people.length; j++) {
+// mostrarCard.innerHTML += `<div class="cards">
+  //  <image class="card_image" src="${data.films[i].people[j].img}">
+   // <h3 class="card_nome"> ${data.films[i].people[j].name}</h3>`
+   // }
+ // }
+//}
+
+// showPeople(); 
+
+
+
