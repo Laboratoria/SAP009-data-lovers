3e6ab9c236ccd408381aeddf51bc823376ed6b20
Stellazen <stellazen.xd@gmail.com>
2023-03-06 14:27:39 -0300

    primeiros testes

--- test/data.spec.js
+++ test/data.spec.js
@@ -1,23 +1,32 @@
-import { example, anotherExample } from '../src/data.js';
+import { ordenar } from '../src/data.js';
+
+const castle = {
+  title: 'Castle in the Sky',
+  release_date:"1986"
+}
+const totoro = {
+  title: 'My neighbor Totoro',
+  release_date:"1988"
+}
+const spirited ={
+  title: 'Spirited Away',
+  release_date: "2001"
+
+}
+const filmes = [totoro, castle, spirited]
+
+it('deve ordenar filmes de A a Z',() =>{
+  const resultado = ordenar(filmes,'deAZ')
+  expect(resultado).toEqual([castle,totoro,spirited])
+})
 
 
-describe('example', () => {
-  it('is a function', () => {
-    expect(typeof example).toBe('function');
-  });
+it('deve ordenar filmes de Z a A',() =>{
+  const resultado = ordenar(filmes,'deZA')
+  expect(resultado).toEqual([spirited, totoro, castle])
+})
 
-  it('returns `example`', () => {
-    expect(example()).toBe('example');
-  });
-});
-
-
-describe('anotherExample', () => {
-  it('is a function', () => {
-    expect(typeof anotherExample).toBe('function');
-  });
-
-  it('returns `anotherExample`', () => {
-    expect(anotherExample()).toBe('OMG');
-  });
-});
+it('deve ordenar data de maior para menor',() =>{
+  const resultado = ordenar(filmes,'deZA')
+  expect(resultado).toEqual([spirited, totoro, castle])
+})
