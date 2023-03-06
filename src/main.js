17657e51a9075911a8efe0782831248e3e93f52b
Edimara Arcanjo <edimaraarcanjo21@gmail.com>
2023-03-06 14:09:18 -0300

    mostrando cards

--- src/main.js
+++ src/main.js
@@ -2,18 +2,18 @@ import data from './data/ghibli/ghibli.js';
 
 const mostrarCard = document.querySelector(".cards")
 
-// function showPeople() {
-// mostrarCard.innerHTML = ""
-// for (let i=0; i < data.films.length ; i++) {
-// for (let j = 0; j <data.films[i].people.length; j++) {
-// mostrarCard.innerHTML += `<div class="cards">
-  //  <image class="card_image" src="${data.films[i].people[j].img}">
-   // <h3 class="card_nome"> ${data.films[i].people[j].name}</h3>`
-   // }
- // }
-//}
+function showPeople() {
+  mostrarCard.innerHTML = ""
+  for (let i=0; i < data.films.length ; i++) {
+    for (let j = 0; j <data.films[i].people.length; j++) {
+      mostrarCard.innerHTML += `<div class="cards">
+    <image class="card_image" src="${data.films[i].people[j].img}">
+    <h3 class="card_nome"> ${data.films[i].people[j].name}</h3>`
+    }
+  }
+}
 
-// showPeople(); 
+showPeople(); 
 
 
 
