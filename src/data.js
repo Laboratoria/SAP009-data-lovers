f1d2fc0a0b2a22bf93a385098277c0c505702fc9
Edimara Arcanjo <edimaraarcanjo21@gmail.com>
2023-03-08 15:17:39 -0300

    arquivos separados

--- src/data.js
+++ src/data.js
@@ -1,14 +1,24 @@
+import data from './data/ghibli/ghibli.js';
 
-
-let generoEscolhido = document.getElementById("genero")
-let nomePersonagem = document.getElementById("nome")
-let especieEscolhida = document.getElementById("especie")
-
-
-export const genero = () => {
-  if (generoEscolhido === "male") {
-    return 
-
+export default function getNamesAndImages() {
+  const NameAndImages = []
+  for (let i = 0; i < data.films.length; i++) {
+    for (let j = 0; j < data.films[i].people.length; j++) {
+      NameAndImages.push({name: `${data.films[i].people[j].name}`, image: `${data.films[i].people[j].img}`})
+    }
   }
-  
-};
+  return NameAndImages
+}
+
+//Como separar Male de Female
+//Usar um filter dentro do gender.push()
+//function getGender() {
+//const gender = []
+//for (let i = 0; i < data.films.length; i++) {
+//for (let j = 0; j < data.films[i].people.length; j++) {
+//if (data.films[i].people[j].gender == "male") {
+//gender.push({name: data.films[i].people[j].name})
+//}
+      
+//}
+//}
--- src/main.js
+++ src/main.js
@@ -1,40 +1,58 @@
-import data from './data/ghibli/ghibli.js';
+import  getNamesAndImages  from "./data.js";
 
 const mostrarCardPai = document.querySelector(".mostrar-card-pai");
 
-function showPeople() {
-  for (let i = 0; i < data.films.length; i++) {
-    for (let j = 0; j < data.films[i].people.length; j++) {
-      const div = document.createElement("div");
-      div.classList.add("cards");
+const NamesAndImg = getNamesAndImages()
 
-      const img = document.createElement("img");
-      img.classList.add("card_image");
-      img.setAttribute("src", data.films[i].people[j].img);
-
-      const nomePersonagem = document.createElement("h3");
-      nomePersonagem.classList.add("card_nome");
-      const nome = document.createTextNode(data.films[i].people[j].name);
-      nomePersonagem.appendChild(nome);
-
-
-      div.appendChild(img);
-      div.appendChild(nomePersonagem);
-
-      mostrarCardPai.appendChild(div);
-
+function showPeople(NamesAndImg) {
+  for (let i = 0; i < NamesAndImg.length; i++) {
     
-    }
+    const div = document.createElement("div");
+    div.classList.add("cards");
+
+    const img = document.createElement("img");
+    img.classList.add("card_image");
+    img.setAttribute("src", NamesAndImg[i].image);
+
+    const nomePersonagem = document.createElement("p");
+    nomePersonagem.classList.add("card_nome");
+    const nome = document.createTextNode(NamesAndImg[i].name);
+    nomePersonagem.appendChild(nome);
+
+    div.appendChild(img);
+    //div.appendChild(nomePersonagem);
+
+    mostrarCardPai.appendChild(div);
+
   }
+    
 }
+showPeople(NamesAndImg);
 
-showPeople();
+const flipcard = document.querySelector('.mostrar-card-pai');
+flipcard.addEventListener('click', function() {
+  flipcard.classList.toggle('flip');
+});
 
-const genero = function filtrarPorGenero(listaPersonagens, genero) {
-  return listaPersonagens.filter(personagem => personagem.gender === genero);
-}
 
+//const generos = function showGender()
 
+// //Fazer as funções = Filtrar e criar
+// export default function filtrarPorGenero() {
+//   console.log("deu certo")
+//   //return listaPersonagens.filter(personagem => personagem.gender === genero);
+// }
+
+// const generoEscolhido = document.getElementById("genero")
+// const nomePersonagem = document.getElementById("nome")
+// const especieEscolhida = document.getElementById("especie")
+
+// //Apenas chamar a função e passar o parâmetro.
+
+// generoEscolhido.addEventListener('change', filtrarPorGenero)
+// //Primeira Função
+// //Criar nossos cards
+// //1 - Nome e Imagem
 
 
 
