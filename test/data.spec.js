551ec7baeb17979eb7a5dc71165408fb70876462
Giselle Schwab Silva <giselleschwab@gmail.com>
2023-03-07 21:11:24 -0300

    teste menos cálculo

--- test/data.spec.js
+++ test/data.spec.js
@@ -1,12 +1,20 @@
-import {filtrarNome, ordemCompletaNome, ordemCompletaNumero} from '../src/data.js';
+import {filtrarNome, ordemCompletaNome, ordemCompletaNumero, filtrarTipo, filtrarRegiao, calcPorcentagem} from '../src/data.js';
 
 const pokeNomes = [
-  {"num": "025", "name": "pikachu", "type":["electric"], "generationName": "kanto"},
-  {"num": "001", "name": "bulbasaur", "type":["grass", "poison"], "generationName": "kanto"},
-  {"num": "007", "name": "squirtle", "type":["water"], "generationName": "kanto"},
-  {"num": "004", "name": "charmander", "type":["fire"], "generationName": "kanto"},
+  {"num": "025", "name": "pikachu", "type":["electric"], "generation": {"name": "kanto"}},
+  {"num": "026", "name": "raichu", "type":["electric"], "generation": {"name": "kanto"}},
+  {"num": "001", "name": "bulbasaur", "type":["grass", "poison"], "generation": {"name": "kanto"}},
+  {"num": "007", "name": "squirtle", "type":["water"], "generation": {"name": "kanto"}},
+  {"num": "004", "name": "charmander", "type":["fire"], "generation": {"name": "kanto"}},
 ];
 
+
+/* const pikachu = {"num": "025", "name": "pikachu", "type":["electric"], "generationName": "kanto"};
+const bulbasaur = {"num": "001", "name": "bulbasaur", "type":["grass", "poison"], "generationName": "kanto"};
+const squirtle = {"num": "007", "name": "squirtle", "type":["water"], "generationName": "kanto"};
+const charmander = {"num": "004", "name": "charmander", "type":["fire"], "generationName": "kanto"};
+
+const pokemonTest = [pikachu, bulbasaur, squirtle, charmander] */
 
 describe('Filtrar nomes Pokémons', () => {
   it('É uma função', () => {
@@ -50,8 +58,42 @@ describe('Ordenar pokémons de forma numérica', () => {
 
   it ('Deve ordernar pokémons de forma numérica descrescente de 251 a 1', () => {
     const ordemDecrescente = ordemCompletaNumero(pokeNomes).reverse();
-    expect(ordemDecrescente[0].num).toBe("025")
+    expect(ordemDecrescente[0].num).toBe("026")
   })
 });
 
+describe('Filtrar pokémons por tipo', () => {
+  it('É uma função', () => {
+    expect (typeof filtrarTipo).toBe('function')
+  });
 
+  it ('Deve retornar um tipo de pokemon', () => {
+    const resultadoFiltro = filtrarTipo(pokeNomes, "fire");
+    const tiposPokes = ["fire"];
+    expect(resultadoFiltro[0].type).toEqual(tiposPokes)
+  })  
+});
+
+describe ('Filtrar pokémon por região', () => {
+  it('É uma função', () => {
+    expect (typeof filtrarRegiao).toBe('function')
+  })
+
+  it ('Deve retornar a região de um pokémon', () => {
+    const resultadoRegiao = filtrarRegiao(pokeNomes, "kanto");
+    const regiaoPoke = "kanto";
+    expect(resultadoRegiao[0].generation.name).toEqual(regiaoPoke)  
+  })
+});
+
+describe ('Calcular a porcentagem de um tipo de pokémon', () => {
+  it('É uma função', () => {
+    expect (typeof calcPorcentagem).toBe('function')
+  })
+
+  it ('Deve retornar a porcentagem de um tipo de pokémon', () => {
+    const resultadoCalculo = calcPorcentagem(pokeNomes, "electric");
+    const porcentagemEletrico = 40;
+    expect(resultadoCalculo).toBe(porcentagemEletrico)  
+  })
+})
