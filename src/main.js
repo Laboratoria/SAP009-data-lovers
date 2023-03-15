9093262a59a72e8db20260d9eb25660243e12b4a
bastosmkarina <kbastoslab09@gmail.com>
2023-03-15 16:56:34 -0300

    Adicionando as alterações para o footer.

--- src/main.js
+++ src/main.js
@@ -1,63 +1,63 @@
 import data from './data/rickandmorty/rickandmorty.js';
 
 function getStatusIcon(character) {
-    let statusIcon = "●"
+  let statusIcon = "●"
 
-    if (character.status === "Alive") {
-        statusIcon = `<span style='color: green'>${statusIcon}</span>` // atributo style  adiciona css a qualquer elemento 
-    } else if (character.status === "Dead") {
-        statusIcon = `<span style='color: red'>${statusIcon}</span>`
-    } else {
-        statusIcon = `<span style='color: gray'>${statusIcon}</span>`
-    }
+  if (character.status === "Alive") {
+    statusIcon = `<span style='color: green'>${statusIcon}</span>` // atributo style  adiciona css a qualquer elemento 
+  } else if (character.status === "Dead") {
+    statusIcon = `<span style='color: red'>${statusIcon}</span>`
+  } else {
+    statusIcon = `<span style='color: gray'>${statusIcon}</span>`
+  }
 
-    return statusIcon
+  return statusIcon
 }
 
 function translateStatus(character) {
-    const current_status = character.status.toLowerCase()
+  const current_status = character.status.toLowerCase()
 
-    const statusTranslation = {
-        'alive': "Viv",
-        'dead': "Mort",
-        'unknown': "Desconhecid"
-    }
+  const statusTranslation = {
+    'alive': "Viv",
+    'dead': "Mort",
+    'unknown': "Desconhecid"
+  }
 
-    let pronoun = ''
-    if (character.gender === 'Male') {
-        pronoun = 'o'
-    } else {
-        pronoun = 'a'
-    }
+  let pronoun = ''
+  if (character.gender === 'Male') {
+    pronoun = 'o'
+  } else {
+    pronoun = 'a'
+  }
 
-    return statusTranslation[current_status] + pronoun
+  return statusTranslation[current_status] + pronoun
 }
 
 function translateSpecies(character) {
-    const current_species = character.species.toLowerCase()
+  const current_species = character.species.toLowerCase()
 
-    const speciesTranslation = {
-        'human': "Humana",
-        'alien': "Alienígena",
-        'humanoid': "Humanoide",
-        'unknown': "Desconhecida",
-        'poopybutthole': "Poopybutthole",
-        'mytholog': "Mytholog",
-        'vampire': "Vampiro",
-        'animal': "Animal",
-        'robot': "Robô",
-        'parasite': "Parasita",
-        'cronenberg': "Cronenberg",
-        'disease': "Disease"
-    }
+  const speciesTranslation = {
+    'human': "Humana",
+    'alien': "Alienígena",
+    'humanoid': "Humanoide",
+    'unknown': "Desconhecida",
+    'poopybutthole': "Poopybutthole",
+    'mytholog': "Mytholog",
+    'vampire': "Vampiro",
+    'animal': "Animal",
+    'robot': "Robô",
+    'parasite': "Parasita",
+    'cronenberg': "Cronenberg",
+    'disease': "Disease"
+  }
 
-    return speciesTranslation[current_species]
+  return speciesTranslation[current_species]
 }
 
 document.querySelector("#main").innerHTML = data
-    .results
-    .map(character => {
-        return `
+  .results
+  .map(character => {
+    return `
     <div class="card">
     <img src="${character.image}"
       class="character-image" />
@@ -70,5 +70,5 @@ document.querySelector("#main").innerHTML = data
   </div>
   </div>
   `
-    }).join('')
+  }).join('')
 
