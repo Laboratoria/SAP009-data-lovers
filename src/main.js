bb70040f50511846f2ce85b03ae1cf1205ec3fbc
michelefeitosa <michelefeitosa08@gmail.com>
2023-04-06 16:32:33 -0300

    ajuste dos cards

--- src/main.js
+++ src/main.js
@@ -1,13 +1,33 @@
-import { example } from './data.js';
 
-const selectChampion = document.getElementById('selectChampion'); 
-const selectOrder = document.getElementById('selectOrder');
+import { filtrarPorNome } from './data.js';
+import data from './data/lol/lol.js';
 
-selectChampion.addEventListener("change", () => {   //pegar o select do Html pra adicionar um evento de Alterar ou escolher 
-    const selectedChampion = selectChampion.value;
-}
+const printarCards = document.getElementById('printarCards')
+const selectChampion = document.getElementById('selectChampion'); //pegar o select do HTML pelo id 
+//const selectOrder = document.getElementById('selectOrder');
 
-selectOrder.addEventListener("change", () => { 
-    const selectedOrder = selectOrder.value;
+console.log(printarCards)
+
+criarCards()
+selectChampion.addEventListener("change", ()=> {  
+  const selecionar = selectChampion.value //pegou o valor que o usuario selecionou 
+  criarCards(); //criar uma função que pegue esse valor e print na tela
+  return selecionar
+});
+
+function criarCards(){  //função que pega os dados e cria o card quando o usuario seleciona
+  const allChampions = data.data 
+
+  for (const champion in allChampions) {
+    const photo = allChampions[champion].splash;
+    const name = allChampions[champion].name;
+    const attack = allChampions[champion].info.attack;
+    const template=(`<section class="cards">
+    <div class='imagem'><img src = '${photo}'></div>
+    <div class='texto'><p>${name}</p><p>Ataque: ${attack}</p>
+    </div></section>`)
+    printarCards.innerHTML += template;
+  }
+  
 }
 
